139a2534680503e25731aed8537f3e1bc4179822
Ian Guerin <img236@nyu.edu>
2013-12-04 16:00:24 -0500

    adding pagination, character limit set, need xml rss stream, need formatting

--- main.py
+++ main.py
@@ -13,10 +13,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 
 import os
 import urllib
+import re
 
 from google.appengine.api import users
 from google.appengine.ext import ndb
@@ -31,40 +31,37 @@ JINJA_ENVIRONMENT = jinja2.Environment(
 
 class MainHandler(webapp2.RequestHandler):
 
-    def get(self):
-        if users.get_current_user():
-          login_url = users.create_logout_url(self.request.uri)
-          login_message = "Logout"
-          user_name = users.get_current_user().nickname()
-          user_id = users.get_current_user().user_id()
-          blogs_query = Blog.query().filter(Blog.author == user_id).order(-Blog.date)
-          blogs = blogs_query.fetch(10)
-          other_blogs_query = Blog.query().filter(Blog.author != user_id).order(Blog.author).order(-Blog.date)
-          other_blogs = other_blogs_query.fetch(10)
-        else:
-          login_url = users.create_login_url(self.request.uri)
-          login_message = "You Must Login"
-          user_name = ""
-          blogs = []
-          other_blogs = []
+  def get(self):
+      if users.get_current_user():
+        login_url = users.create_logout_url(self.request.uri)
+        login_message = "Logout"
+        user_name = users.get_current_user().nickname()
+        user_id = users.get_current_user().user_id()
+        blogs_query = Blog.query().filter(Blog.author == user_id).order(-Blog.date)
+        blogs = blogs_query.fetch(10)
+        other_blogs_query = Blog.query().filter(Blog.author != user_id).order(Blog.author).order(-Blog.date)
+        other_blogs = other_blogs_query.fetch(10)
+      else:
+        login_url = users.create_login_url(self.request.uri)
+        login_message = "You Must Login"
+        user_name = ""
+        blogs = []
+        other_blogs = []
 
-        values = {
-          "blogs": blogs,
-          "other_blogs": other_blogs,
-          "login_url": login_url,
-          "login_message": login_message,
-          "user_name": user_name
-        }
+      values = {
+        "blogs": blogs,
+        "other_blogs": other_blogs,
+        "login_url": login_url,
+        "login_message": login_message,
+        "user_name": user_name
+      }
 
-        template = JINJA_ENVIRONMENT.get_template("templates/index.html")
-        self.response.write(template.render(values))
+      template = JINJA_ENVIRONMENT.get_template("templates/index.html")
+      self.response.write(template.render(values))
 
 class BlogHandler(webapp2.RequestHandler):
 
-  def get(self):
-    last_slash = self.request.url.rfind("/") + 1
-    blog_name = self.request.url[last_slash::]
-
+  def get(self,blog_name):
     if users.get_current_user() and blog_name != "":
       login_url = users.create_logout_url(self.request.uri)
       login_message = "Logout"
@@ -74,20 +71,32 @@ class BlogHandler(webapp2.RequestHandler):
       self.redirect("/")
       return
 
-    blog_query = Blog.query(ancestor=blog_key(user_id)).order(-Blog.date)
+    # it would be nice if this would work  
+    #blog_query = Blog.query(ancestor=blog_key(user_id)).order(-Blog.date)
+    blog_query = Blog.query(ancestor=blog_key(user_id)).filter(Blog.name == blog_name)
     blog = blog_query.get()
 
-    query = BlogPost.query(ancestor=blog_post_key(blog_name)).order(-BlogPost.date)
-    blog_posts = query.fetch(10)
+    if not blog:
+      blog_query = Blog.query().filter(Blog.name == blog_name)
+      blog = blog_query.get()      
+
+    query = Article.query(ancestor=article_key(blog_name)).order(-Article.date)
+    articles = query.fetch(10)
+
+    for article in articles[:]:
+      if len(article.content) > 500:
+        article.abbreviated_content = article.content[:497] + "..."
+      else:
+        article.abbreviated_content = article.content
 
     values = {
       "blog": blog,
-      "blog_posts": blog_posts,
+      "articles": articles,
       "login_url": login_url,
       "login_message": login_message,
       "user_name": user_name,
       "user_id": user_id,
-      "blog_name": blog_name
+      "blog_name": blog_name,
     }
 
     template = JINJA_ENVIRONMENT.get_template("templates/blog.html")
@@ -126,14 +135,15 @@ class AddBlogHandler(webapp2.RequestHandler):
 
     self.redirect("/blog/" + blog_name)
 
-def blog_post_key(blog_name):
+def article_key(blog_name):
   return ndb.Key("Blog", blog_name)
 
-class BlogPost(ndb.Model):
+class Article(ndb.Model):
 
   author = ndb.StringProperty()
   author_name = ndb.StringProperty()
-  title = ndb.StringProperty(indexed=False)
+  blog_name = ndb.StringProperty()
+  title = ndb.StringProperty()
   content = ndb.StringProperty(indexed=False)
   tags = ndb.StringProperty(indexed=False)
   date = ndb.DateTimeProperty(auto_now_add=True)
@@ -144,43 +154,44 @@ class WriteHandler(webapp2.RequestHandler):
     blog_name = self.request.get("blog_name")
 
     if users.get_current_user() and blog_name != "":
-      blog_post = BlogPost(parent=blog_post_key(blog_name))
-      blog_post.author = users.get_current_user().user_id()
-      blog_post.author_name = users.get_current_user().nickname()
+      article = Article(parent=article_key(blog_name))
+      article.author = users.get_current_user().user_id()
+      article.author_name = users.get_current_user().nickname()
+      article.blog_name = blog_name
     else:
       self.redirect("/")
       return
 
-    blog_post.title = self.request.get("title")
-    blog_post.content = self.request.get("content")
-    blog_post.put()
+    article.title = self.request.get("title")
+    article.content = self.request.get("content")
+    article.put()
 
-    self.redirect("/blog/" + blog_name)
+    self.redirect("/blog/" + blog_name + "/article/" + article.title);
 
 class ArticleHandler(webapp2.RequestHandler):
 
-  def get(self):
-    # last_slash = self.request.url.rfind("/") + 1
-    # blog_name = self.request.url[last_slash::]
+  def get(self, blog_name, article_name):
+    if users.get_current_user() and blog_name != "":
+      login_url = users.create_logout_url(self.request.uri)
+      login_message = "Logout"
+      user_id = users.get_current_user().user_id()
+      user_name = users.get_current_user().nickname()
+    else:
+      self.redirect("/")
+      return
 
+    article_query = Article.query(ancestor=article_key(blog_name)).filter(Article.title == article_name)
+    article = article_query.get()
 
-    # if users.get_current_user() and blog_name != "":
-    #   login_url = users.create_logout_url(self.request.uri)
-    #   login_message = "Logout"
-    #   user_name = users.get_current_user()
-    # else:
-    #   self.redirect("/")
-    #   return
-
-    # values = {
-    #   "login_url": login_url,
-    #   "login_message": login_message,
-    #   "user_name": user_name,
-    #   "blog_name": blog_name
-    # }
     values = {
-          "troll": "alot"
-        }
+      "login_url": login_url,
+      "login_message": login_message,
+      "user_name": user_name,
+      "user_name": user_name,
+      "user_id": user_id,
+      "blog_name": blog_name,
+      "article": article
+    }
 
     template = JINJA_ENVIRONMENT.get_template("templates/article.html")
     self.response.write(template.render(values)) 
@@ -188,7 +199,7 @@ class ArticleHandler(webapp2.RequestHandler):
 app = webapp2.WSGIApplication([
   ("/addblog", AddBlogHandler),
   ("/blog/.*/write", WriteHandler),
-  ("/blog/article/.*", ArticleHandler),
-  ("/blog/.*", BlogHandler),
+  ("/blog/(.*)/article/(.*)", ArticleHandler),
+  ("/blog/(.*)", BlogHandler),
   ("/.*", MainHandler)
 ], debug=True)
