aced040a85e760e425b4c8a07647fd6fc911c2a4
Ian Guerin <img236@nyu.edu>
2013-12-10 00:54:12 -0500

    need documentation and tags on homescreen, everything else appears to be fulfilled

--- main.py
+++ main.py
@@ -31,55 +31,53 @@ def find_links(text):
 class MainHandler(webapp2.RequestHandler):
 
   def get(self):
-    page_size = 2
+    page_size = 10
     if users.get_current_user():
       login_url = users.create_logout_url(self.request.uri)
       login_message = "Logout"
       user_name = users.get_current_user().nickname()
       user_id = users.get_current_user().user_id()
       
-      blogs_page_number = self.request.get("bpage")        
-      if not blogs_page_number:
-        blogs_page_number = 1
-      else:
-        blogs_page_number = int(blogs_page_number)
-
-      blogs_query = Blog.query().filter(Blog.author == user_id).order(-Blog.date)
-      blogs = blogs_query.fetch(page_size, offset=((blogs_page_number - 1) * page_size))
-      
-      all_blogs_page_number = self.request.get("opage")
-      if not all_blogs_page_number:
-        all_blogs_page_number = 1
-      else:
-        all_blogs_page_number = int(all_blogs_page_number)
-
-      all_blogs_query = Blog.query().order(-Blog.date)
-      all_blogs_count_query = Blog.query().order(-Blog.date)
-      all_blogs = all_blogs_query.fetch(page_size, offset=((all_blogs_page_number - 1) * page_size))
-
-      blogs_count = blogs_query.count()
-      all_blogs_count = all_blogs_count_query.count()
-
-      if blogs_page_number * page_size < blogs_count:
-        blogs_more_articles = True
-      else:
-        blogs_more_articles = False
-
-      if all_blogs_page_number * page_size < all_blogs_count:
-        all_blogs_more_articles = True
-      else:
-        all_blogs_more_articles = False
-
     else:
       login_url = users.create_login_url(self.request.uri)
       login_message = "Login"
       user_name = ""
-      blogs = []
-      all_blogs = []
-      blogs_more_articles = None
-      blogs_page_number = None
-      all_blogs_page_number = None
-      all_blogs_more_articles = None
+      user_id = None
+
+
+    
+    blogs_page_number = self.request.get("bpage")        
+    if not blogs_page_number:
+      blogs_page_number = 1
+    else:
+      blogs_page_number = int(blogs_page_number)
+
+    blogs_query = Blog.query().filter(Blog.author == user_id).order(-Blog.date)
+    blogs = blogs_query.fetch(page_size, offset=((blogs_page_number - 1) * page_size))
+    
+    all_blogs_page_number = self.request.get("opage")
+    if not all_blogs_page_number:
+      all_blogs_page_number = 1
+    else:
+      all_blogs_page_number = int(all_blogs_page_number)
+
+    all_blogs_query = Blog.query().order(-Blog.date)
+    all_blogs_count_query = Blog.query().order(-Blog.date)
+    all_blogs = all_blogs_query.fetch(page_size, offset=((all_blogs_page_number - 1) * page_size))
+
+    blogs_count = blogs_query.count()
+    all_blogs_count = all_blogs_count_query.count()
+
+    if blogs_page_number * page_size < blogs_count:
+      blogs_more_articles = True
+    else:
+      blogs_more_articles = False
+
+    if all_blogs_page_number * page_size < all_blogs_count:
+      all_blogs_more_articles = True
+    else:
+      all_blogs_more_articles = False
+
 
     values = {
       "blogs": blogs,
@@ -99,18 +97,20 @@ class MainHandler(webapp2.RequestHandler):
 class BlogHandler(webapp2.RequestHandler):
 
   def get(self,blog_name):
-    page_size = 2
+    page_size = 10
     if users.get_current_user() and blog_name != "":
       login_url = users.create_logout_url(self.request.uri)
       login_message = "Logout"
       user_name = users.get_current_user().nickname()
       user_id = users.get_current_user().user_id()
+      blog_query = Blog.query(ancestor=blog_key(user_id)).filter(Blog.name == blog_name)
+      blog = blog_query.get()
     else:
-      self.redirect("/")
-      return
-
-    blog_query = Blog.query(ancestor=blog_key(user_id)).filter(Blog.name == blog_name)
-    blog = blog_query.get()
+     login_url = users.create_login_url(self.request.uri)
+     login_message = "Login"
+     user_name = ""
+     user_id = None
+     blog = None
 
     page_number = self.request.get("page")
     if not page_number:
@@ -198,20 +198,90 @@ class Article(ndb.Model):
   title = ndb.StringProperty()
   content = ndb.StringProperty(indexed=False)
   tags = ndb.StringProperty(repeated=True)
-  date = ndb.DateTimeProperty(auto_now_add=True)
+  date = ndb.DateTimeProperty(auto_now=True)
+  creation_date = ndb.DateTimeProperty(auto_now_add=True)
 
 class AddArticleHandler(webapp2.RequestHandler):
 
   def post(self):
     blog_name = self.request.get("blog_name")
 
-    if users.get_current_user() and blog_name != "":
+    article_query = Article.query().filter(Article.title == self.request.get("title"))
+    article = article_query.get()
+
+    if users.get_current_user() and blog_name != "" and self.request.get("title") != "" and article is None:
       article = Article(parent=article_key(blog_name))
       article.author = users.get_current_user().user_id()
       article.author_name = users.get_current_user().nickname()
       article.blog_name = blog_name
     else:
       self.redirect("/")
+      return
+
+    article.title = self.request.get("title")
+    article.content = self.request.get("content")
+    article.tags = self.request.get("tags").split(",")
+    for i in range(0, len(article.tags)):
+      article.tags[i] = article.tags[i].lower().strip()
+
+    if article.title == "" or article.content == "":
+      self.redirect("/blog/" + blog_name)
+      return
+
+    article.put()
+
+    self.redirect("/blog/" + blog_name + "/article/" + article.title)
+
+class EditArticleHandler(webapp2.RequestHandler):
+
+  def get(self, blog_name, article_int_id):
+    if users.get_current_user() and blog_name != "":
+      login_url = users.create_logout_url(self.request.uri)
+      login_message = "Logout"
+      user_id = users.get_current_user().user_id()
+      user_name = users.get_current_user().nickname()
+      blog_query = Blog.query(ancestor=blog_key(user_id)).filter(Blog.name == blog_name)
+      blog = blog_query.get()
+    else:
+      login_url = users.create_login_url(self.request.uri)
+      login_message = "Login"
+      user_name = ""
+      user_id = None
+
+    article = Article.get_by_id(int(article_int_id), parent=article_key(blog_name))
+
+    if article.author != user_id:
+      self.redirect("/blog/" + blog_name + "/article/" + article.title)
+      return
+
+    if not blog:
+      blog_query = Blog.query().filter(Blog.name == blog_name)
+      blog = blog_query.get()  
+
+    values = {
+      "login_url": login_url,
+      "login_message": login_message,
+      "user_name": user_name,
+      "user_name": user_name,
+      "user_id": user_id,
+      "blog": blog,
+      "article": article
+    }
+
+    template = JINJA_ENVIRONMENT.get_template("templates/edit-article.html")
+    self.response.write(template.render(values)) 
+
+  def post(self, blog_name, article_int_id):
+    blog_name = self.request.get("blog_name")
+    article_id = self.request.get("article-id")
+
+    if users.get_current_user() and blog_name != "":
+      article = Article.get_by_id(int(article_int_id), parent=article_key(blog_name))
+      article.author = users.get_current_user().user_id()
+      article.author_name = users.get_current_user().nickname()
+      article.blog_name = blog_name
+    else:
+      self.redirect("/shitbag")
       return
 
     article.title = self.request.get("title")
@@ -236,15 +306,17 @@ class ArticleHandler(webapp2.RequestHandler):
       login_message = "Logout"
       user_id = users.get_current_user().user_id()
       user_name = users.get_current_user().nickname()
+      blog_query = Blog.query(ancestor=blog_key(user_id)).filter(Blog.name == blog_name)
+      blog = blog_query.get()
     else:
-      self.redirect("/")
-      return
+      login_url = users.create_login_url(self.request.uri)
+      login_message = "Login"
+      user_name = ""
+      user_id = None
+      blog = None
 
     article_query = Article.query(ancestor=article_key(blog_name)).filter(Article.title == article_name)
     article = article_query.get()
-
-    blog_query = Blog.query(ancestor=blog_key(user_id)).filter(Blog.name == blog_name)
-    blog = blog_query.get()
 
     if not blog:
       blog_query = Blog.query().filter(Blog.name == blog_name)
@@ -268,15 +340,17 @@ class ArticleHandler(webapp2.RequestHandler):
 class TagHandler(webapp2.RequestHandler):
 
   def get(self, tag_name):
-    page_size = 2
+    page_size = 10
     if users.get_current_user() and tag_name != "":
       login_url = users.create_logout_url(self.request.uri)
       login_message = "Logout"
       user_name = users.get_current_user().nickname()
       user_id = users.get_current_user().user_id()
     else:
-      self.redirect("/")
-      return
+      login_url = users.create_login_url(self.request.uri)
+      login_message = "Login"
+      user_name = ""
+      user_id = None
 
     page_number = self.request.get("page")
     if not page_number:
@@ -334,8 +408,10 @@ class ImageHandler(webapp2.RequestHandler):
       user_name = users.get_current_user().nickname()
       user_id = users.get_current_user().user_id()
     else:
-      self.redirect("/")
-      return
+      login_url = users.create_login_url(self.request.uri)
+      login_message = "Login"
+      user_name = ""
+      user_id = None
 
     query = Image.query().order(-Image.date).filter(Image.author == user_id and Image.filename == filename)
     images = query.fetch()
@@ -381,7 +457,7 @@ class ImageHandler(webapp2.RequestHandler):
 class ImagePageHandler(webapp2.RequestHandler):
 
   def get(self):
-    page_size = 2
+    page_size = 10
     if users.get_current_user():
       login_url = users.create_logout_url(self.request.uri)
       login_message = "Logout"
@@ -432,14 +508,14 @@ class RSSHandler(webapp2.RequestHandler):
       login_message = "Logout"
       user_name = users.get_current_user().nickname()
       user_id = users.get_current_user().user_id()
+      blog_query = Blog.query(ancestor=blog_key(user_id)).filter(Blog.name == blog_name)
+      blog = blog_query.get()
     else:
-      self.redirect("/")
-      return
+      user_name = ""
+      user_id = None
+      blog = None
 
     link = os.path.split(self.request.url)[0][:-3]
-
-    blog_query = Blog.query(ancestor=blog_key(user_id)).filter(Blog.name == blog_name)
-    blog = blog_query.get()
 
     if not blog:
       blog_query = Blog.query().filter(Blog.name == blog_name)
@@ -461,6 +537,7 @@ class RSSHandler(webapp2.RequestHandler):
 app = webapp2.WSGIApplication([
   ("/addblog", AddBlogHandler),
   ("/blog/.*/addarticle", AddArticleHandler),
+  ("/blog/(.*)/article/(.*)/edit", EditArticleHandler),
   ("/blog/(.*)/article/(.*)", ArticleHandler),
   ("/image", ImagePageHandler),
   ("/image/(.*)", ImageHandler),
